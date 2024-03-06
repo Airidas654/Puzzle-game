85a3c82fe7e5966ad4291e940fe602bf05481086
Airidas <airidasleonavicius@gmail.com>
2024-03-07 00:04:06 +0200

    player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10;
     Rigidbody2D rg;
+    Animator animator;
     [HideInInspector] public Vector2 moveDir;
     void Start()
     {
         rg = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalRaw = Input.GetAxisRaw("Horizontal");
+        float verticalRaw = Input.GetAxisRaw("Vertical");
+
+        animator.SetBool("Walking", horizontalRaw!=0||verticalRaw!=0);
+
+        if (horizontalRaw < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (horizontalRaw > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         float mag = inputVector.magnitude;
         if (mag > 1)
