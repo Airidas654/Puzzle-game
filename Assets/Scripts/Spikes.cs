3fdcd8c7e5a09aefe955c1337f4e26d3b59dbd42
Airidas654 <airidasleonavicius@gmail.com>
2024-03-07 00:16:03 +0200

    Spikes more

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -8,12 +8,16 @@ public class Spikes : Receiver
     [SerializeField] float spikeTime;
     [SerializeField] LayerMask triggerMask;
     [SerializeField] bool isTimed;
+    [SerializeField] bool isSpikesUp = false;
+    [SerializeField] Sprite openSpikesSprite;
+    [SerializeField] Sprite closedSpikesSprite;
+    private bool timerOn = false;
     float tempTime;
     // Start is called before the first frame update
     void Start()
     {
         
-        SpikesControl(state);
+        SpikesControl(isSpikesUp);
         
         tempTime = spikeTime;
     }
@@ -21,29 +25,32 @@ public class Spikes : Receiver
     // Update is called once per frame
     void Update()
     {
-        tempTime -= Time.deltaTime;
-        if(tempTime <= 0)
+        if (isTimed && timerOn)
         {
-            state = !state;
-            SpikesControl(state);
-            tempTime = spikeTime;
+            tempTime -= Time.deltaTime;
+            if (tempTime <= 0)
+            {
+                isSpikesUp = !isSpikesUp;
+                SpikesControl(isSpikesUp);
+                tempTime = spikeTime;
+            }
         }
+        
         
     }
     public void SpikesControl(bool control)
     {
         if (control)
         {
-            OnStateOn();
+            SpikesOn();
             //Debug.Log("Spikehjnhuhyubhyb");
         }
         else
         {
-            OnStateOff();
+            SpikesOff();
         }
     }
-
-    public override void OnStateOn()
+    public void SpikesOn()
     {
         var triggerCollider = Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0, triggerMask);
         foreach (var col in triggerCollider)
@@ -52,20 +59,48 @@ public class Spikes : Receiver
             {
                 return;
             }
+
         }
-        if(triggerCollider.Length == 1) {
+        GetComponent<SpriteRenderer>().sprite = openSpikesSprite;
+        if (triggerCollider.Length == 1)
+        {
             Debug.Log("You died");
             return;
         }
         var Collider = GetComponent<Collider2D>();
         Collider.enabled = true;
+    }
+    public void SpikesOff()
+    {
+        GetComponent<SpriteRenderer>().sprite = closedSpikesSprite;
+        var Collider = GetComponent<Collider2D>();
+        Collider.enabled = false;
+    }
+    public override void OnStateOn()
+    {
+        if (!isTimed)
+        {
+            SpikesOn();
+        }
+        else
+        {
+            timerOn = true;
+        }
 
 
     }
     public override void OnStateOff()
     {
-        var Collider = GetComponent<Collider2D>();
-        Collider.enabled = false;
+        if (!isTimed)
+        {
+            SpikesOff();
+        }
+        else
+        {
+            timerOn = false;
+            SpikesOff();
+            tempTime = spikeTime;
+        }
     }
 
 }
