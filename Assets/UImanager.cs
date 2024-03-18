5700b9d249fa7646e4e3629a9eee5a1700409263
Airidas654 <airidasleonavicius@gmail.com>
2024-03-18 19:27:06 +0200

    dark

---
+++ Assets/UImanager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class UImanager : MonoBehaviour
+{
+    [SerializeField] Image dark;
+    [SerializeField] float darkStartLength;
+    public static UImanager Instance = null;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    static void StartLevelTransition(int sceneIndex, float length, float delay = 0)
+    {
+        Instance.dark.DOKill();
+        Instance.dark.gameObject.SetActive(true);
+        Instance.dark.DOColor(new Color(0, 0, 0, 1), length).SetDelay(delay).SetEase(Ease.InSine).OnComplete(() => SceneManager.LoadScene(sceneIndex));
+    }
+
+    private void Start()
+    {
+        dark.DOKill();
+        dark.DOColor(new Color(0,0,0,0), darkStartLength).SetEase(Ease.OutSine).OnComplete(()=>dark.gameObject.SetActive(false));
+    }
+
+    private void OnPreRender()
+    {
+        Debug.Log("ya");
+        if (dark != null)
+        {
+            
+            dark.gameObject.SetActive(true);
+            Color col = new Color(0, 0, 0, 1);
+            dark.color = col;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
