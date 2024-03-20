acb52df725b12763e931b8836c0c95599f8d1eba
armandassss <armandasleonavicius@gmail.com>
2024-03-20 22:09:45 +0200

    Main menu

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,42 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Door : Receiver 
+public class Door : MonoBehaviour
 {
     [SerializeField] Sprite OpenedDoorSprite;
     [SerializeField] Sprite ClosedDoorSprite;
+    [SerializeField] Vector2 openingColliderOffset;
+    [SerializeField] float lengthToOpen;
+    [SerializeField] LayerMask layersToOpen;
+    [SerializeField] bool goToIncrimentedLevel;
+    [SerializeField] int exactLevel;
     bool opened;
-    bool goToIncrimentedLevel;
-    int exactLevel;
-
 
     private void Start()
     {
-        if (state)
-        {
-            OnStateOn();
-        }
-        else
-        {
-            OnStateOff();
-        }
+        opened = false;
     }
 
-    public override void OnStateOn()
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere((Vector2)transform.position+openingColliderOffset,lengthToOpen);
+    }
+
+    void Open()
     {
         GetComponent<SpriteRenderer>().sprite = OpenedDoorSprite;
-        opened = true;
     }
 
-    public override void OnStateOff()
+    void Close()
     {
         GetComponent<SpriteRenderer>().sprite = ClosedDoorSprite;
-        opened = false;
+    }
+
+    private void Update()
+    {
+        Collider2D hit = Physics2D.OverlapCircle((Vector2)transform.position+openingColliderOffset, lengthToOpen,layersToOpen);
+        if (hit != null && !opened)
+        {
+            Open();
+            opened = true;
+        }else if(hit == null && opened)
+        {
+            Close();
+            opened = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player" && opened)
+        if(collision.tag == "Player")
         {
             if (goToIncrimentedLevel)
             {
