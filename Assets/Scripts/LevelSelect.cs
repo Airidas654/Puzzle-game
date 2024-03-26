20295f84c8a85386b8423b9a0955347ac409bc0b
armandassss <armandasleonavicius@gmail.com>
2024-03-26 16:48:49 +0200

    Messages working???

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,21 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelSelect : MonoBehaviour
 {
     [SerializeField] int levelId;
+    [SerializeField] GameObject messagePrefab;
     Switch trans;
+    float time;
+    GameObject message;
+
+    public void SetTimer(float time)
+    {
+        this.time = time;
+        message.SetActive(true);
+    }
 
     private void Start()
     {
         trans = GetComponent<Switch>();
+
+        message = Instantiate(messagePrefab);
+        message.GetComponent<TextMeshProUGUI>().text = string.Format("Level {0}", levelId - 2);
+        Camera mCamera = Camera.main;
+        message.transform.SetParent(GameObject.Find("Canvas").transform);
+
+        Vector2 adjustedPosition = mCamera.WorldToScreenPoint(transform.position);
+        RectTransform mCanvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+
+        adjustedPosition.x *= mCanvas.rect.width / (float)mCamera.pixelWidth;
+        adjustedPosition.y *= mCanvas.rect.height / (float)mCamera.pixelHeight;
+
+        message.GetComponent<RectTransform>().anchoredPosition = adjustedPosition - mCanvas.sizeDelta / 2f;
+        message.SetActive(false);
     }
 
     void Update()
     {
         if (trans.defaultState) {
             UImanager.StartLevelTransition(levelId, 0.5f);
+        }
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            if(time <= 0)
+            {
+                time = 0;
+                message.SetActive(false);
+            }
         }
     }
 }
---
+++ Assets/Scripts/MessageManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    [SerializeField] float messageDist;
+    [SerializeField] float timeToDisappear;
+    [SerializeField] Vector2 messageOffset;
+    
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,messageDist);
+    }
+
+    void Update()
+    {
+        Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, messageDist);
+        foreach(Collider2D h in hit)
+        {
+            LevelSelect ls;
+            if (h.TryGetComponent(out ls))
+            {
+                ls.SetTimer(timeToDisappear);
+            }
+        }
+    }
+}
