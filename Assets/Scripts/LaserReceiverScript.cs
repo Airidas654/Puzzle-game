74604b165dedb028c92cf36e1c8d95ab5f7ed8f4
Mat3xx <funksss25@gmail.com>
2024-05-14 21:18:29 +0300

    receivers

--- Assets/Scripts/LaserReceiverScript.cs
+++ Assets/Scripts/LaserReceiverScript.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class LaserReceiverScript : LogicObject
 {
     [SerializeField] private bool oneTimePower = false;
+    [SerializeField] private Sprite receiverOn;
+    [SerializeField] private Sprite receiverOff;
 
     public void SetValue(bool value)
     {
-        if (oneTimePower) Transmit(true);
-        else Transmit(value);
+        if (oneTimePower)
+        {
+         Transmit(true);
+            gameObject.GetComponent<SpriteRenderer>().sprite=receiverOn;
+        }
+        else
+        {
+            Transmit(value);
+			gameObject.GetComponent<SpriteRenderer>().sprite =(value)? receiverOn: receiverOff;
+		}
     }
 }
